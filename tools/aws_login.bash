af2817248c63b2d33549350f1d7b752c7a9eb198
Riliumph <riliumph@gmail.com>
2026-09-09 10:48:25 +0000

    chore: 表示情報を追加

--- tools/aws_login.bash
+++ tools/aws_login.bash
@@ -14,9 +14,16 @@ aws-login() {
             --token-code "${token}" \
             --output json)
 
+    local expiration
+    expiration=$(echo "$creds" | jq -r '.Credentials.Expiration')
     export AWS_ACCESS_KEY_ID=$(echo "$creds" | jq -r '.Credentials.AccessKeyId')
     export AWS_SECRET_ACCESS_KEY=$(echo "$creds" | jq -r '.Credentials.SecretAccessKey')
     export AWS_SESSION_TOKEN=$(echo "$creds" | jq -r '.Credentials.SessionToken')
+
     echo "AWS session credentials have been exported."
+    echo "ACCESS KEY   : ${AWS_ACCESS_KEY_ID}"
+    echo "SECRET KEY   : ${AWS_SECRET_ACCESS_KEY}"
+    echo "SESSION TOKEN: ${AWS_SESSION_TOKEN}"
+    echo "Expires at   : $expiration"
 
 }
