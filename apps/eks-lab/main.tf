505f250317a02b3714d132453c7522854ff25215
Riliumph <riliumph@gmail.com>
2026-09-16 16:03:31 +0900

    feat: private route tableを構築

--- apps/eks-lab/main.tf
+++ apps/eks-lab/main.tf
@@ -29,14 +29,17 @@ resource "aws_subnet" "this" {
 #===================================================================
 # Route Table
 
+#===================================================================
+# Public Route Table
+
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
+
   tags = {
     Name = "eks-public-rt"
   }
 }
 
-# Route Table Record
 resource "aws_route" "internet" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
@@ -51,4 +54,25 @@ resource "aws_route_table_association" "public" {
 
   subnet_id      = aws_subnet.this[each.key].id
   route_table_id = aws_route_table.public.id
+}
+
+#===================================================================
+# Private Route Table
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "eks-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  for_each = {
+    for k, v in var.subnet : k => v
+    if !v.public
+  }
+
+  subnet_id      = aws_subnet.this[each.key].id
+  route_table_id = aws_route_table.private.id
 }
